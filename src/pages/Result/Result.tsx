645d4a22d3706ff948f85df2f38174ef570f076c
박인국 <pkook7@gmail.com>
2023-12-21 21:09:20 +0900

    결과수정

--- src/pages/Result/Result.tsx
+++ src/pages/Result/Result.tsx
@@ -1,109 +1,255 @@
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
-const Result=()=> {
+const Result = () => {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
-  const resultNumber = Number(searchParams.get('r')) ?? '0';
-  
+  const resultNumber = Number(searchParams.get("r")) ?? "0";
+
   let chickenName;
   let chickenImage;
+  let chickenInfo;
 
-  if(resultNumber===1){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===2){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===3){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===4){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===5){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===6){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===7){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===8){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===9){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===10){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===11){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===12){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===13){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===14){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===15){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===16){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===17){chickenName='60계치킨 하하핫 치킨'; chickenImage='/Images/하하핫치킨.png'}
-  if(resultNumber===18){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===19){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===20){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===21){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===22){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===23){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===24){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===25){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===26){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===27){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===28){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===29){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===30){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===31){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-  if(resultNumber===32){chickenName='BHC 황금올리브 치킨'; chickenImage='/Images/chicken1.png'}
-
-
+  if (resultNumber === 1) {
+    chickenName = "굽네 고추바사삭 순살";
+    chickenInfo = "매콤바삭";
+    chickenImage = "/Images/굽네고추바사삭.png";
+  }
+  if (resultNumber === 2) {
+    chickenName = "굽네 고추바사삭 순살 곱빼기";
+    chickenInfo = "매콤바삭";
+    chickenImage = "/Images/굽네고추바사삭.png";
+  }
+  if (resultNumber === 3) {
+    chickenName = "굽네 오리지널 순살";
+    chickenInfo = "오븐구이 치킨의 원조";
+    chickenImage = "/Images/굽네오리지널.png";
+  }
+  if (resultNumber === 4) {
+    chickenName = "굽네 오리지널 순살+치트킹(콘치즈)";
+    chickenInfo = "만능 시즈닝과 세트!";
+    chickenImage = "/Images/굽네오지리널치트킹.png";
+  }
+  if (resultNumber === 5) {
+    chickenName = "굽네 고추바사삭";
+    chickenInfo = "매콤바삭";
+    chickenImage = "/Images/굽네고추바사삭.png";
+  }
+  if (resultNumber === 6) {
+    chickenName = "굽네 고추바사삭 곱빼기";
+    chickenInfo = "매콤바삭";
+    chickenImage = "/Images/굽네고추바사삭.png";
+  }
+  if (resultNumber === 7) {
+    chickenName = "굽네 오리지널";
+    chickenInfo = "오븐구이 치킨의 원조";
+    chickenImage = "/Images/굽네오리지널.png";
+  }
+  if (resultNumber === 8) {
+    chickenName = "굽네 오리지널+치트킹(콘치즈)";
+    chickenInfo = "만능 시즈닝과 세트!";
+    chickenImage = "/Images/굽네오리지널치트킹.png";
+  }
+  if (resultNumber === 9) {
+    chickenName = "굽네 볼케이노 순살";
+    chickenInfo = "중독성 있는 불맛";
+    chickenImage = "/Images/굽네볼케이노.png";
+  }
+  if (resultNumber === 10) {
+    chickenName = "굽네 볼케이노 순살";
+    chickenInfo = "중독성 있는 불맛";
+    chickenImage = "/Images/굽네볼케이노.png";
+  }
+  if (resultNumber === 11) {
+    chickenName = "굽네 갈비천왕 순살";
+    chickenInfo = "단짠단짠의 정석";
+    chickenImage = "/Images/굽네갈비천왕.png";
+  }
+  if (resultNumber === 12) {
+    chickenName = "굽네 갈비천왕 순살";
+    chickenInfo = "단짠단짠의 정석";
+    chickenImage = "/Images/굽네갈비천왕.png";
+  }
+  if (resultNumber === 13) {
+    chickenName = "굽네 불금치킨";
+    chickenInfo = "매콤한 양념소스에 버터갈릭 소스까지";
+    chickenImage = "/Images/굽네불금치킨.png";
+  }
+  if (resultNumber === 14) {
+    chickenName = "BBQ 자메이카 통다리구이";
+    chickenInfo = "통다리에 자메이카 저크소스!";
+    chickenImage = "/Images/BBQ자메이카통다리.png";
+  }
+  if (resultNumber === 15) {
+    chickenName = "굽네 갈비천왕";
+    chickenInfo = "단짠단짠의 정석";
+    chickenImage = "/Images/굽네갈비천왕.png";
+  }
+  if (resultNumber === 16) {
+    chickenName = "굽네 갈비천왕";
+    chickenInfo = "단짠단짠의 정석";
+    chickenImage = "/Images/굽네갈비천왕.png";
+  }
+  if (resultNumber === 17) {
+    chickenName = "BHC HOT 후라이드치킨 순살";
+    chickenInfo = "명불허전! 바삭함, 고소함 가득!";
+    chickenImage = "/Images/BHC핫후라이드.jpg";
+  }
+  if (resultNumber === 18) {
+    chickenName = "BBQ HOT황금올리브치킨 크리스피 순살";
+    chickenInfo = "겉은 바삭 육즙 가득";
+    chickenImage = "/Images/BBQ핫황금올리브치킨크리스피순살.png";
+  }
+  if (resultNumber === 19) {
+    chickenName = "BBQ 황금올리브치킨 순살";
+    chickenInfo = "겉은 바삭 육즙 가득";
+    chickenImage = "/Images/BBQ황금올리브치킨.png";
+  }
+  if (resultNumber === 20) {
+    chickenName = "BBQ 크런치 순살크래커";
+    chickenInfo = "황금올리브 닭다리살에, 바삭한 순살 치킨";
+    chickenImage = "/Images/BBQ크런치순살크래커.png";
+  }
+  if (resultNumber === 21) {
+    chickenName = "BHC HOT 후라이드치킨";
+    chickenInfo = "명불허전! 바삭함, 고소함 가득!";
+    chickenImage = "/Images/BHC핫후라이드.png";
+  }
+  if (resultNumber === 22) {
+    chickenName = "BBQ HOT황금올리브치킨 크리스피";
+    chickenInfo = "겉은 바삭 육즙 가득";
+    chickenImage = "/Images/BBQ핫황금올리브치킨크리스피순살.png";
+  }
+  if (resultNumber === 23) {
+    chickenName = "BBQ 황금올리브치킨";
+    chickenInfo = "겉은 바삭 육즙 가득";
+    chickenImage = "/Images/BBQ황금올리브치킨.png";
+  }
+  if (resultNumber === 24) {
+    chickenName = "BBQ 황금올리브치킨콤보";
+    chickenInfo = "닭다리, 윙, 봉으로 이루어진 후라이드의 대명사";
+    chickenImage = "/Images/BBQ황금올리브치킨콤보.png";
+  }
+  if (resultNumber === 25) {
+    chickenName = "BHC 레드킹 순살";
+    chickenInfo = "매콤달콤!";
+    chickenImage = "/Images/BBQ레드킹순살.jpg";
+  }
+  if (resultNumber === 26) {
+    chickenName = "푸라닭 고추마요 순살";
+    chickenInfo = "단짠단짠! 숙성간장과 꿀의 황금비율";
+    chickenImage = "/Images/푸라닭고추마요.png";
+  }
+  if (resultNumber === 27) {
+    chickenName = "BHC 골드킹 순살";
+    chickenInfo = "계속 손이 갈 수 밖에 없는 마법의 치킨";
+    chickenImage = "/Images/BHC골드킹순살.png";
+  }
+  if (resultNumber === 28) {
+    chickenName = "푸라닭 블랙알리오 순살";
+    chickenInfo = "깊고 진한 간장과 고소하면서 담백한 마늘의 만남!";
+    chickenImage = "/Images/푸라닭블랙알리오.png";
+  }
+  if (resultNumber === 29) {
+    chickenName = "BHC 레드킹";
+    chickenInfo = "매콤달콤!";
+    chickenImage = "/Images/BHC레드킹.jpg";
+  }
+  if (resultNumber === 30) {
+    chickenName = "푸라닭 고추마요 순살";
+    chickenInfo = "단짠단짠! 숙성간장과 꿀의 황금비율";
+    chickenImage = "/Images/푸라닭고추마요.png";
+  }
+  if (resultNumber === 31) {
+    chickenName = "BHC 골드킹";
+    chickenInfo = "계속 손이 갈 수 밖에 없는 마법의 치킨";
+    chickenImage = "/Images/BHC골드킹.png";
+  }
+  if (resultNumber === 32) {
+    chickenName = "푸라닭 블랙알리오";
+    chickenInfo = "깊고 진한 간장과 고소하면서 담백한 마늘의 만남!";
+    chickenImage = "/Images/푸라닭블랙알리오.png";
+  }
 
   return (
     <ResultStyle>
-    <Title>당신과 어울리는 치킨은?</Title>
-    <ResultName>{chickenName}</ResultName>
-    <ResultPic src={chickenImage} alt="사진" />
+      <Title>당신과 어울리는 치킨은?</Title>
+      <ResultName>{chickenName}</ResultName>
+      <ResultInfo>{chickenInfo}</ResultInfo>
+      <ResultPic src={chickenImage} alt="사진" />
 
-    <ChoiceBox>
-    <ResultBtn  onClick={()=>{navigate('/')}}>테스트 다시하기</ResultBtn>
-    <ResultBtn  onClick={()=>{navigate('/')}}>결과 공유하기</ResultBtn>
-    </ChoiceBox>
+      <ChoiceBox>
+        <ResultBtn
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          테스트 다시하기
+        </ResultBtn>
+        <ResultBtn
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          결과 공유하기
+        </ResultBtn>
+      </ChoiceBox>
     </ResultStyle>
   );
-}
+};
 
 export default Result;
 
 const ResultStyle = styled.div`
-  margin-top:100px;
-  display:flex;
+  margin-top: 50px;
+  display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
 const Title = styled.div`
   text-align: center;
-  font-size:55px;
-  font-weight:700;
+  font-size: 35px;
+  font-weight: 500;
 `;
 
 const ResultName = styled.div`
-margin-top:40px;
-    text-align: center;
-  font-size:45px;
-  font-weight:700;
+  margin-top: 50px;
+  text-align: center;
+  font-size: 45px;
+  font-weight: 700;
 `;
 
+const ResultInfo = styled.div`
+  margin-top: 30px;
+  text-align: center;
+  font-size: 25px;
+  font-weight: 500;
+`;
 
 const ResultPic = styled.img`
-margin-top:15px;
+  margin-top: 15px;
   width: 500px;
+  height: 350px;
+  object-fit: cover;
 `;
 
 const ChoiceBox = styled.div`
-  width:550px;
+  width: 550px;
 `;
-
 
 const ResultBtn = styled.div`
-margin-top: 30px;
-text-align: center;
-font-weight: 700;
-font-size: 30px;
-background-color: #FFC436;
-width: 100%;
-padding:15px 0;
-border-radius: 5px;
-cursor: pointer;
+  margin-top: 30px;
+  text-align: center;
+  font-weight: 700;
+  font-size: 30px;
+  background-color: #ffc436;
+  width: 100%;
+  padding: 15px 0;
+  border-radius: 5px;
+  cursor: pointer;
   &:hover {
-    background-color: #FFD966;
+    background-color: #ffd966;
   }
 `;
-
